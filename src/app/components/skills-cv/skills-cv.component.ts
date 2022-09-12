d3a912ad40a497bab4a23b88e31e2bfbf45aa35f
Adpsec <asp.security@protonmail.com>
2022-09-12 00:45:19 -0500

    Creación y configuración del componene skills-cv close #10

--- src/app/components/skills-cv/skills-cv.component.ts
+++ src/app/components/skills-cv/skills-cv.component.ts
@@ -7,9 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsCvComponent implements OnInit {
 
+  skills : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
+
+    let sk = {
+      element: "Trabajo en equipo",
+      
+    };
+    let sk1 = {
+      element: "Microgestion",
+      
+    };
+    let sk2 = {
+      element: "Orientado a objetivos",
+      
+    };
+    let sk3 = {
+      element: "Centrado en el futuro",
+      
+    };
+    let sk4 = {
+      element: "Pensamiento Crítico",
+      
+    };
+    let sk5 = {
+      element: "Resiliencia",
+      
+    };
+    let sk6 = {
+      element: " Comunicación verbal y escrita",
+      
+    };
+    let sk7 = {
+      element: "Liderazgo",
+      
+    };
+    let sk8 = {
+      element: "Creatividad",
+      
+    };
+    let sk9 = {
+      element: "Disposición de tiempo",
+      
+    };
+    this.skills.push(sk);
+    this.skills.push(sk1);
+    this.skills.push(sk2);
+    this.skills.push(sk3);
+    this.skills.push(sk4);
+    this.skills.push(sk5);
+    this.skills.push(sk6);
+    this.skills.push(sk7);
+    this.skills.push(sk8);
+    this.skills.push(sk9);
+
   }
 
 }
